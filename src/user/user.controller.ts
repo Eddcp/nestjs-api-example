8e88b21f60686afe8e6fa9b5c02c5bbb85d7d143
eddcp <eduardocspv@gmail.com>
2022-04-27 05:31:51 -0300

    Redirecting with decorator Redirect

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Redirect, Version } from '@nestjs/common';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller({
+  path: 'user',
+})
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
+  @Version('1')
+  @Redirect('http://localhost:3000/v2/user', 302)
   getUser() {
     return this.userService.getUser();
+  }
+
+  @Get()
+  @Version('2')
+  getUserV2() {
+    return 'Redirected successfully';
+  }
+
+  @Post()
+  saveUser() {
+    return 'Created';
   }
 }
